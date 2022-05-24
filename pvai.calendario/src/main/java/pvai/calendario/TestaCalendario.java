72f26508825fe3679304946e443ccb27e1793687
Flag-fix <isac.bandeir4@gmail.com>
2022-05-23 22:40:06 -0300

    First commit

---
+++ pvai.calendario/src/main/java/pvai/calendario/TestaCalendario.java
@@ -0,0 +1,19 @@
+package pvai.calendario;
+
+import paranavai.calendario.Calendario;
+
+public class TestaCalendario {
+	
+	public static void main(String[] args) {
+		//Calendario.mostrarCalendario(); //V(1)
+		//Calendario.mostrarCalendario("12","12"); //V(1)
+		//Calendario.mostrarCalendario("e"); //V(1)
+		//Calendario.mostrarCalendario("0"); //V(1)
+		//Calendario.mostrarCalendario("10000"); //V(1)
+		//Calendario.mostrarCalendario("2022"); //V(1)
+		// Calendario.mostrarCalendario("5,6","7,8"); //V(1)
+		 Calendario.mostrarCalendario("5,6","0"); //V(1)
+	}
+	
+
+}
